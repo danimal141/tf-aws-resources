a6d170db16f940100dd4fc477862a1b92275dd56
danimal141 <hideaki.ishii1204@gmail.com>
2020-04-30 16:47:45 +0900

    Add security groups

--- vpc.tf
+++ vpc.tf
@@ -55,3 +55,29 @@ resource "aws_route_table_association" "sample_public_subnet1" {
   subnet_id = aws_subnet.sample_public_subnet1.id
   route_table_id = aws_route_table.sample_route_table.id
 }
+
+resource "aws_security_group" "sample_security_group" {
+  vpc_id = aws_vpc.sample_vpc.id
+
+  tags = {
+    Name = "sample_security_group"
+  }
+}
+
+resource "aws_security_group_rule" "in_ssh" {
+  security_group_id = aws_security_group.sample_security_group.id
+  type = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+}
+
+resource "aws_security_group_rule" "out_all" {
+  security_group_id = aws_security_group.sample_security_group.id
+  type = "egress"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port = 0
+  to_port = 0
+  protocol = "all"
+}
