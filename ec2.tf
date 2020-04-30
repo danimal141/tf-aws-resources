2269443635967b8b91b983c7e5f5b4fdbd2d2a03
danimal141 <hideaki.ishii1204@gmail.com>
2020-04-30 17:39:04 +0900

    Fix invalid config

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample_instance" {
   ami = "ami-785c491f" # Ubuntu 16.04 LTS official ami
   vpc_security_group_ids = [aws_security_group.ec2_ins_security_group.id]
-  subnet_id = aws_subnet.sample_private_subnet1.id
+  subnet_id = aws_subnet.sample_public_subnet1.id
   key_name = aws_key_pair.sample_key_pair.id
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
